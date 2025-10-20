7879c896c975728a3999c69edef7490b50dc1e46
MatheusLima2803 <matheuslima23@discente.ufg.br>
2025-10-19 23:26:06 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,18 +1,44 @@
 import rpyc
-from constRPYC import * #-
-from rpyc.utils.server import ThreadedServer
 
-class DBList(rpyc.Service):
-  value = []
+class ServicoLista(rpyc.Service):
+    def on_connect(self, conexao):
+        self.lista = []
 
-  def exposed_append(self, data):
-    self.value = self.value + [data]
-    return self.value
+    def exposed_valor(self):
+        return self.lista
 
-  def exposed_value(self):
-    return self.value
+    def exposed_adicionar(self, elemento):
+        self.lista.append(elemento)
+        return f"{elemento} adicionado com sucesso."
+
+    def exposed_inserir(self, indice, elemento):
+        if 0 <= indice <= len(self.lista):
+            self.lista.insert(indice, elemento)
+            return f"{elemento} inserido na posição {indice}."
+        else:
+            return "Índice inválido."
+
+    def exposed_remover(self, elemento):
+        try:
+            self.lista.remove(elemento)
+            return f"{elemento} removido com sucesso."
+        except ValueError:
+            return "Elemento não encontrado."
+
+    def exposed_pesquisar(self, elemento):
+        try:
+            pos = self.lista.index(elemento)
+            return f"{elemento} encontrado na posição {pos}."
+        except ValueError:
+            return "Elemento não encontrado."
+
+    def exposed_ordenar(self):
+        self.lista.sort()
+        return "Lista ordenada com sucesso."
 
 if __name__ == "__main__":
-  server = ThreadedServer(DBList(), port = PORT)
-  server.start()
-
+    from constRPYC import HOST, PORTA
+    from rpyc.utils.server import ThreadedServer
+    servidor = ThreadedServer(ServicoLista, port=PORTA, hostname=HOST)
+    print(f"Servidor RPyC ativo em {HOST}:{PORTA}")
+    servidor.start()
